7f21e05255ef50f47bb3f7e8c9d50edb0cd34755
Danilo Pianini <danilo.pianini@unibo.it>
2021-05-14 16:36:23 +0200

    Fix bug inverting CPD and checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -248,7 +248,7 @@ tasks.register("blame") {
                 when (root.tagName) {
                     "pmd" -> PmdQAInfoExtractor(root)
                     "pmd-cpd" -> CpdQAInfoExtractor(root)
-                    "checkstyle" -> CpdQAInfoExtractor(root)
+                    "checkstyle" -> CheckstyleQAInfoExtractor(root)
                     "BugCollection" -> SpotBugsQAInfoExtractor(root)
                     else -> emptyList<QAInfo>().also { println("Unknown root type ${root.tagName}")}
                 }
