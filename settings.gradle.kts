2a2dd523143542c6158f6a7beda90a324c41403f
Danilo Pianini <danilo.pianini@unibo.it>
2024-06-03 15:24:42 +0200

    switch to develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    id("com.gradle.enterprise") version "3.17.4"
+    id("com.gradle.develocity") version "3.17.4"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishAlways()
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
         buildScanPublished {
             file("scan-journal.log").writeText("$buildScanId - $buildScanUri\n")
         }
